0123f5e4b8deecc169377719d32df14b1c6ab645
Gerda <gerdabrodin@gmail.com>
2025-05-29 20:13:43 +0200

    api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,17 @@
-import axios from 'axios';
+/*import axios from 'axios';
 
 const authApi = axios.create({
   baseURL: 'https://localhost:7006/api',
   withCredentials: true
+});
+
+export default authApi;
+*/
+import axios from 'axios';
+
+const authApi = axios.create({
+  baseURL: 'https://your-auth-service.azurewebsites.net/api',
+  withCredentials: true // keep this if you're using cookies
 });
 
 export default authApi;
