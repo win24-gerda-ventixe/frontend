ba823a30753b16608ad2f3f24c7106fc670b9c8d
Gerda <gerdabrodin@gmail.com>
2025-06-03 20:34:49 +0200

    added interceptors to  profileApi

--- src/profileApi.js
+++ src/profileApi.js
@@ -1,15 +1,22 @@
 import axios from 'axios';
 
-const API_URL = 'https://ventixe-gerda-webapp3.azurewebsites.net/api';
 
-const token = localStorage.getItem('token');
+const API_URL = 'https://ventixe-gerda-webapp3.azurewebsites.net/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
-    Authorization: `Bearer ${token}`,
     'Content-Type': 'application/json'
   }
+});
+
+// This runs before each request:
+axiosInstance.interceptors.request.use(config => {
+  const token = localStorage.getItem('token'); // Get fresh token each time
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 const profileApi = {
